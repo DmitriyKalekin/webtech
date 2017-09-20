e972760af3f7f6adda586eca5595a474158f2474
KalekinDmitry <herr_horror@mail.ru>
2017-09-20 14:08:56 +0600

    Update FactoryRouting.class.php

--- core/FactoryRouting.class.php
+++ core/FactoryRouting.class.php
@@ -2,21 +2,50 @@
 namespace core;
 class FactoryRouting 
 {
-    static function getController($uri) : \core\Controller
+    static function getController($uri) :  array //\core\Controller
     {
       $parts = explode("/", $uri);
       
-      if (!empty($parts[1]) )
+      $method_name = "index";
+      
+      if (!empty($parts[2]))
       {
-        
-       $cname = "\\core\\".UCFirst($parts[1])."Controller";
-       
-       return new $cname();
+        $method_name = $parts[2];
         
       }
-
-      return new \core\C404Controller();
       
+      $controller = null;
+      
+       $cname = "\\core\\".UCFirst(@$parts[1])."Controller";
+       $file_name = str_replace("\\", "/",$cname.".class.php");
+      
+     
+    
+      
+      if (!empty($parts[1]) && file_exists( getcwd().$file_name))
+      {
+                
+              $controller =  new $cname();
+       
+      }
+      else
+      {
+
+      $controller =  new \core\C404Controller();
+      }
+      
+      
+      
+      if (method_exists($controller, $method_name))
+      {
+        
+       
+        return array($controller, $method_name);        
+      }
+      else
+      {
+        throw new \Exception("Error! Warning! Achtung! Danger!");
+      }
       
     }
 }
