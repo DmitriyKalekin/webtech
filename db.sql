b07a258034594b6f4e9590d5235fbcf2a1aeb8eb
KalekinDmitry <herr_horror@mail.ru>
2017-09-20 14:00:24 +0600

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,22 @@
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT '',
+  `age` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `age`) VALUES
+(1, 'Иван Иванов', 45),
+(2, 'Петр Петров', 46),
+(3, 'Сидор Сидоров', 37),
+(4, 'Никита Джигурда', 55),
+(5, 'Дмитрий Медведев', 40),
+(6, 'Вася Васильев', 33);
