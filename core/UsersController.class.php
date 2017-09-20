395651e6a2aa236d16b28c655ae77829ac6aa9e3
KalekinDmitry <herr_horror@mail.ru>
2017-09-20 14:12:22 +0600

    Update UsersController.class.php

--- core/UsersController.class.php
+++ core/UsersController.class.php
@@ -1,17 +1,27 @@
 <?php
 namespace core;
 
-class UsersController extends \core\Controller {
-  public function getPage()
+class UsersController extends \core\Controller 
+{
+  public  $model = null; //\core\UsersModel
+  
+  public function __construct()
   {
-    //echo "<h1>Users page</h1>";
-    $arr = array();
-    $arr["id"] = 5;
-    $arr["images"] = array();
-    $arr["images"][] = array("id"=>7, "url"=>"госуслуги.ру");
-    $arr["images"][] = array("id"=>8, "url"=>"yandex.ru");
+    $this->model = new \core\UsersModel();
     
-    echo json_encode($arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+  }
+  
+  public function index()
+  {
+    $data = $this->model->getAll();
+    
+    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+  }
+  
+  public function delete()
+  {
+    echo "<h1>Deleting user</h1>";
+   
   }
   
 }
